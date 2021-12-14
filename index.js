b4908f14a94e22dffa5cfcecf699d1364bbe5c0e
Christian E <christianelena92@gmail.com>
2021-12-13 20:28:03 -0600

    Challenge : TwoNumberSum question

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
- 
+// TWO Number Sum
+
+// Q. write a fn
+// input : non-empty array of Integers, integer to represent a Target-sum.
+// if any two numbers in the input array sum up to the target sum,
+// output : return the nums in an array
+// if, no two numbers sum up to the target sum, the fn should return an empty array.
+
+// NOte: Target Sum has to be obtained by summing two diffrent ints in the array; you can't add a single int to itself in order to obtain the target sum.
+// There will be at one pair up most.
+// The numbers could also be in reverse order
+
+// ex:
+// arr = [3, 5, -4, 8, 11, 1, -1, 6];
+// tagetSum = 10;
+
+function twoNumberSum(arr, targetSum) {}
