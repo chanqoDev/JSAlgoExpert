4b6a2918a41f1589354675f4f9fab09034ad5b11
Christian E <christianelena92@gmail.com>
2022-06-10 22:19:55 -0500

    ADD: sorted_Squared_Array challenge file

---
+++ Sorted_Squared_Array/sortedsqrtArr.js
@@ -0,0 +1,6 @@
+// Sorted Squared Array
+// Write a function that takes in a non-empty array of integers that are sorted in ascending order.
+// and retunrs a new array of the same length with the squares of the original integers also soreted in ascending order.
+
+// input: array of integers "sorted in ascending order"
+// output: new arr of the same length with the squares of the original ints ( same ascending order)
