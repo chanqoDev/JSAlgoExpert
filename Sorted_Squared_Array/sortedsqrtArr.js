16d892b05c0892375ece3521b2dc63bebbde9049
Christian E <christianelena92@gmail.com>
2022-06-10 22:50:51 -0500

    ADD: brute force solution to sortedsquared array challenge

--- Sorted_Squared_Array/sortedsqrtArr.js
+++ Sorted_Squared_Array/sortedsqrtArr.js
@@ -4,3 +4,17 @@
 
 // input: array of integers "sorted in ascending order"
 // output: new arr of the same length with the squares of the original ints ( same ascending order)
+// Time: 0(nlogn) || space: 0(n) :: where n is the length of the input arr.
+const map = (arr, cbf) => {
+  const output = [];
+  for (let i = 0; i < arr.length; i++) {
+    let newElem = cbf(arr[i], i, arr);
+    output.push(newElem);
+  }
+  return output;
+};
+
+const sortedSquaredArr = (arr) => {
+  const result = arr.map((num) => num ** 2);
+  result.sort((a, b) => a - b);
+};
