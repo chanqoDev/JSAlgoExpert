a2b46bcef6d93ea7d396f8c27df1b4c22d270add
Christian E <christianelena92@gmail.com>
2022-06-12 19:25:26 -0500

    ADD. Smallest Difference Dir and Challenge description

---
+++ Smallest__Difference/app.js
@@ -0,0 +1,13 @@
+// Challenge : Smallest Difference
+
+/**
+ * Write a func() that takes in two arrays of integers that are non-empty.
+ * Finds the pair of numbers ( one from each array) whos absolute difference is closest to zero. (and)
+ * it returns an array contianing these two numbers, with the number from the first array in the first position.
+ *
+ * NOte: that the absolute difference of two ints is the distance between them on the real number line.
+ * Exmaple: the absolute diffenrence of -5 and 5 is 10, and the absolute difference of -5 and -4 is 1.
+ *
+ *
+ * Assume that there will only be one pair of numbers with the smallest difference.
+ */
