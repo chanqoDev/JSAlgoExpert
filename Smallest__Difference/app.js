0f2c8f4baa8ea81048d760b24c2b4b1aaaeb0cb8
Christian E <christianelena92@gmail.com>
2022-06-17 20:35:20 -0500

    ADD: solution for Smallest Absolute Difference

--- Smallest__Difference/app.js
+++ Smallest__Difference/app.js
@@ -11,3 +11,42 @@
  *
  * Assume that there will only be one pair of numbers with the smallest difference.
  */
+
+// Time 0(nLog(n) + mLog(m)) time | 0(1) space
+const smallestDifference = (arrayOne, arrayTwo) => {
+  // sort array
+  arrayOne.sort((a, b) => a - b);
+  arrayTwo.sort((a, b) => a - b);
+  // set pointers our indicies
+  let idxOne = 0;
+  let idxTwo = 0;
+  let smallest = Infinity;
+  let current = Infinity;
+  let smallestPair = [];
+  while (idxOne < arrayOne.length && idxTwo < arrayTwo.length) {
+    // declare our first & second numbers varialbe to hold and compare the value
+    let firstNum = arrayOne[idxOne];
+    let secondNum = arrayTwo[idxTwo];
+    // if fistnum < second
+    if (firstNum < secondNum) {
+      current = secondNum - firstNum;
+      idxOne++;
+    } else if (secondNum < firstNum) {
+      current = firstNum - secondNum;
+      idxTwo++;
+    } else {
+      return [firstNum, secondNum];
+    }
+    if (smallest > current) {
+      smallest = current;
+      smallestPair = [firstNum, secondNum];
+    }
+  }
+  return smallestPair;
+};
+
+const arrayOne = [-1, 5, 10, 20, 28, 3]; // [ -1, 3, 5, 10, 20, 28 ]
+const arrayTwo = [26, 134, 135, 15, 17]; // [ 15, 17, 26, 134, 135 ]
+
+const output = smallestDifference(arrayOne, arrayTwo);
+console.log(output);
