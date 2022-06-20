bf5f4a96ab4016a03a83c9cae7dab222049c59d7
Christian E <christianelena92@gmail.com>
2022-06-19 21:24:21 -0500

    UPDATE: better solution and time and space complexity

--- Smallest__Difference/app.js
+++ Smallest__Difference/app.js
@@ -13,36 +13,51 @@
  */
 
 // Time 0(nLog(n) + mLog(m)) time | 0(1) space
-const smallestDifference = (arrayOne, arrayTwo) => {
-  // sort array
-  arrayOne.sort((a, b) => a - b);
-  arrayTwo.sort((a, b) => a - b);
-  // set pointers our indicies
-  let idxOne = 0;
-  let idxTwo = 0;
-  let smallest = Infinity;
-  let current = Infinity;
-  let smallestPair = [];
-  while (idxOne < arrayOne.length && idxTwo < arrayTwo.length) {
-    // declare our first & second numbers varialbe to hold and compare the value
-    let firstNum = arrayOne[idxOne];
-    let secondNum = arrayTwo[idxTwo];
-    // if fistnum < second
-    if (firstNum < secondNum) {
-      current = secondNum - firstNum;
-      idxOne++;
-    } else if (secondNum < firstNum) {
-      current = firstNum - secondNum;
-      idxTwo++;
-    } else {
-      return [firstNum, secondNum];
-    }
-    if (smallest > current) {
-      smallest = current;
-      smallestPair = [firstNum, secondNum];
+// const smallestDifference = (arrayOne, arrayTwo) => {
+// arrayOne.sort((a, b) => a - b);
+// arrayTwo.sort((a, b) => a - b);
+
+// let idxOne = 0;
+// let idxTwo = 0;
+// let smallest = Infinity;
+// let current = Infinity;
+// let smallestPair = [];
+// while (idxOne < arrayOne.length && idxTwo < arrayTwo.length) {
+//     let firstNum = arrayOne[idxOne];
+//     let secondNum = arrayTwo[idxTwo];
+//     if (firstNum < secondNum) {
+//     current = secondNum - firstNum;
+//     idxOne++;
+//     } else if (secondNum < firstNum) {
+//     current = firstNum - secondNum;
+//     idxTwo++;
+//     } else {
+//     return [firstNum, secondNum];
+//     }
+//     if (smallest > current) {
+//     smallest = current;
+//     smallestPair = [firstNum, secondNum];
+//     }
+// }
+// return smallestPair;
+// };
+
+// SOLUTION 2: BEST SOlution
+const smallestDifference = (arr1, arr2) => {
+  let bestPair = [],
+    smallestDiff = Infinity;
+
+  for (const num1 of arr1) {
+    for (const num2 of arr2) {
+      let difference = Math.abs(num1 - num2);
+
+      if (difference < smallestDiff) {
+        smallestDiff = difference;
+        bestPair = [num1, num2];
+      }
     }
   }
-  return smallestPair;
+  return bestPair;
 };
 
 const arrayOne = [-1, 5, 10, 20, 28, 3]; // [ -1, 3, 5, 10, 20, 28 ]
